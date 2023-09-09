92d2b2f73f7706d18fac09cd4d481585e7b8f4d3
chainika2757 <chainika2757>
2023-09-09 23:49:56 +0530

    Add Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(){
+  runApp(FlutterApp());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
- 
+class FlutterApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build (BuildContext context){
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
+      title:"FlutterApp",
+       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-       
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 47, 10, 110)),
-        useMaterial3: true,
+        primarySwatch:Colors.blue
       ),
-      home: const MyHomePage(title: 'To-Do-Tasks'),
+      home: DashboardScreen(),
     );
   }
-}
+    }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-     
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-  
-    return Scaffold(
-      appBar: AppBar(
-       
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-       
-        title: Text(widget.title),
-      ),
-      body: Center(
-       
-        child: Column(
-        
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    class DashboardScreen extends StatelessWidget{
+     @override
+     Widget build(BuildContext context){
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("To-Do-Tasks"),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+        body: Container(
+          color:Colors.white,
+        ),
+      );  }
 }
